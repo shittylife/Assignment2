4c11ffb599c5e795336c4d3f72016b2fb356263a
hercules2123 <a1523709@gmail.com>
2022-04-06 22:21:10 +0800

    test

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.assignment2;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+
+        button.setText("testButton");
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                button.setText("testButton1");
+            }
+        });
     }
 }
+
