07b466843ba1cdd327d994f0b5d8ccd1a8a69858
Anonymous <anonymoussakuraikk@gmail.com>
2022-04-20 02:58:32 +0800

    test

--- app/src/main/java/com/example/assignment2/MainActivity.java
+++ app/src/main/java/com/example/assignment2/MainActivity.java
@@ -1,30 +1,47 @@
 package com.example.assignment2;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.util.Log;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener{
+
+    private FrameLayout layout;
+    private final TaskFragment taskFragment = new TaskFragment();
+    private final CalendarFragment calendarFragment = new CalendarFragment();
+    private final TimerFragment timerFragment = new TimerFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        layout = findViewById(R.id.fragment);
+        BottomNavigationView navigationView = findViewById(R.id.navigationBar);
+        navigationView.setOnItemSelectedListener(this);
+    }
 
-        Button button = findViewById(R.id.button);
-
-        button.setText("testButton");
-
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                button.setText("testButton1");
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.task: {
+                getSupportFragmentManager().beginTransaction().replace(layout.getId(), taskFragment).commit();
+                return true;
             }
-        });
+            case R.id.calendar: {
+                getSupportFragmentManager().beginTransaction().replace(layout.getId(), calendarFragment).commit();
+                return true;
+            }
+            case R.id.timer: {
+                getSupportFragmentManager().beginTransaction().replace(layout.getId(), timerFragment).commit();
+                return true;
+            }
+        }
+        return false;
     }
 }
 
