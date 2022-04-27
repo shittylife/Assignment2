6e00272930b071d9802e5c75e27bc25d59652b68
Michaexxx2324 <michaelwong88041356@gmail.com>
2022-04-28 00:35:36 +0800

    2

--- app/src/main/java/com/example/assignment2/Calendar2Fragment.java
+++ app/src/main/java/com/example/assignment2/Calendar2Fragment.java
@@ -1,18 +1,41 @@
 package com.example.assignment2;
 
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link Calendar2Fragment#newInstance} factory method to
  * create an instance of this fragment.
  */
+
 public class Calendar2Fragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
@@ -23,6 +46,18 @@ public class Calendar2Fragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private DatePickerDialog datePickerDialog;
+    private Button dateButton,save,pick;
+    private Spinner sp1;
+    private String date,dateDay,dateMonth;
+    private TextView tvtest;
+    private DatePicker datePicker;
+    private EditText detail,et2;
+    private List<Task> data = new ArrayList<>();
+    SharedPreferences sharedpreferences;
+    public static final String MYPreference = "MYPref";
+    public static final String Name = "nameKey";
+    public static final String Email = "emailKey";
 
     public Calendar2Fragment() {
         // Required empty public constructor
@@ -58,7 +93,204 @@ public class Calendar2Fragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar2, container, false);
+        View view = inflater.inflate(R.layout.fragment_calendar2, container, false);
+        initDatePicker();
+        dateButton = view.findViewById(R.id.datePickerButton);
+        tvtest = view.findViewById(R.id.tvtest);
+        save = view.findViewById(R.id.btn4);
+        sp1 = view.findViewById(R.id.sp1);
+        pick = view.findViewById(R.id.btn5);
+        detail = view.findViewById(R.id.et1);
+        et2 = view.findViewById(R.id.et2);
+        datePicker = view.findViewById(R.id.DatePicker);
+        dateButton.setText(getTodaysDate());
+        DatePicker picker = view.findViewById(R.id.DatePicker);
+
+
+
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),R.array.Type, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        sp1.setAdapter(adapter);
+        sp1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+                String choice = adapterView.getItemAtPosition(i).toString();
+                Toast.makeText(getActivity(),choice,Toast.LENGTH_SHORT).show();
+
+                if(choice == "Custom"){
+                    et2.setVisibility(View.VISIBLE);
+
+                }
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
+
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(tvtest == null && sp1== null){
+                    Toast.makeText(getActivity(),"Please fill in the Date and Theme.",Toast.LENGTH_SHORT).show();
+                }else {
+                    Intent intent = new Intent(getActivity(), MainActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(getActivity(), "Saved",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+        dateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker.setVisibility(View.VISIBLE);
+                pick.setVisibility(View.VISIBLE);
+
+
+
+            }
+        });
+
+        pick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                datePicker.setVisibility(View.GONE);
+                pick.setVisibility(View.GONE);
+                sp1.setVisibility(View.VISIBLE);
+                tvtest.setText("Selected Day: " + date);
+                Toast.makeText(getActivity(),date + "Selected",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        int Year = picker.getYear();
+        int Month =picker.getMonth()+1;
+        int Day = picker.getDayOfMonth();
+        picker.init(Year,Month,Day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+                StringBuilder builder = new StringBuilder();
+                builder.append(day+"/");
+                builder.append(month+1+"/");
+                builder.append(year);
+                date=builder.toString();
+                dateDay=String.valueOf(picker.getMonth()+1);
+                dateMonth=String.valueOf(picker.getDayOfMonth());
+            }
+        });
+        tvtest.setText(date);
+
+        return view;
     }
+
+
+
+
+
+    private String getTodaysDate()
+    {
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        month = month + 1;
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        return makeDateString(day, month, year);
+    }
+
+    private void initDatePicker()
+    {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener()
+        {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day)
+            {
+                month = month + 1;
+                String date = makeDateString(day, month, year);
+                dateButton.setText(date);
+            }
+        };
+
+        Calendar cal = Calendar.getInstance();
+        int year = cal.get(Calendar.YEAR);
+        int month = cal.get(Calendar.MONTH);
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+
+        int style = AlertDialog.THEME_HOLO_LIGHT;
+
+        datePickerDialog = new DatePickerDialog(getActivity(), style, dateSetListener, year, month, day);
+        //datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+
+    }
+
+    private String makeDateString(int day, int month, int year)
+    {
+        return getMonthFormat(month) + " " + day + " " + year;
+    }
+
+    private String getMonthFormat(int month)
+    {
+        if(month == 1)
+            return "JAN";
+        if(month == 2)
+            return "FEB";
+        if(month == 3)
+            return "MAR";
+        if(month == 4)
+            return "APR";
+        if(month == 5)
+            return "MAY";
+        if(month == 6)
+            return "JUN";
+        if(month == 7)
+            return "JUL";
+        if(month == 8)
+            return "AUG";
+        if(month == 9)
+            return "SEP";
+        if(month == 10)
+            return "OCT";
+        if(month == 11)
+            return "NOV";
+        if(month == 12)
+            return "DEC";
+
+        //default should never happen
+        return "JAN";
+    }
+
+    public void openDatePicker(View view)
+    {
+        datePickerDialog.show();
+    }
+
+    private void update() {
+        JSONArray array = new JSONArray();
+        for (int i = 0; i < data.size(); i++) {
+            if (!data.get(i).getTitle().isEmpty()&&!data.get(i).getText().isEmpty()) {
+                JSONObject object = new JSONObject();
+                try {
+                    object.put("date", data.get(i).getId());
+                    object.put("theme", data.get(i).getTitle());
+                    object.put("detail", data.get(i).getText());
+                    object.put("Price", data.get(i).getDay());
+                    object.put("Type", data.get(i).getText());
+                    //object.put("title", data.get(i).getTitle());
+                    //.put("text", data.get(i).getText());
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                array.put(object);
+            }
+        }
+        PreferenceManager.getDefaultSharedPreferences(getContext()).edit().putString("C", array.toString()).apply();
+    }
+
+
 }
--- app/src/main/java/com/example/assignment2/CalendarFragment.java
+++ app/src/main/java/com/example/assignment2/CalendarFragment.java
@@ -1,13 +1,31 @@
 package com.example.assignment2;
 
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class CalendarFragment extends Fragment {
+
+    private List<Task> data = new ArrayList<>();
+    ImageButton iv2;
+    TextView tv5;
+    Calendar2Fragment calendar2Fragment = new Calendar2Fragment();
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,6 +35,32 @@ public class CalendarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+        iv2 = view.findViewById(R.id.iv2);
+
+        try {
+            JSONArray array = new JSONArray(PreferenceManager.getDefaultSharedPreferences(getContext()).getString("C", "[]"));
+            for (int i = 0; i < array.length(); i++) {
+                JSONObject object = array.getJSONObject(i);
+                Task task = new Task(object.getLong("id"), object.getInt("day"), object.getInt("month"), object.getInt("year"), object.getString("title"), object.getString("text"));
+                data.add(task);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        iv2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction = getFragmentManager().beginTransaction();
+                transaction.replace(R.id.fragment,calendar2Fragment );
+                transaction.commit();
+            }
+        });
+
+
+        tv5.getText()
+
+
         return view;
     }
 }
