d1da217df1340067d378a6c4a78fce3384cc7b9b
Michaexxx2324 <michaelwong88041356@gmail.com>
2022-04-27 22:48:15 +0800

    1

--- app/src/main/java/com/example/assignment2/CalendarFragment.java
+++ app/src/main/java/com/example/assignment2/CalendarFragment.java
@@ -4,10 +4,13 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
 
 public class CalendarFragment extends Fragment {
+
+    ImageButton iv2;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,6 +20,13 @@ public class CalendarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+
+
+
+
+
+
         return view;
     }
 }
